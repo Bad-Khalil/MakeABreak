43ab71b7122e5e3da73fe2784b523c9b58686df2
Michael Lucas <info@michael-lucas.net>
2019-04-01 17:32:54 +0200

    Update Darstellung

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,21 @@
-const {app, ipcRenderer} = require('electron')
+const {
+   app,
+   ipcRenderer
+} = require('electron')
 
-ipcRenderer.on('message', function(event, text) {    
-   $("#test").html(text);
+ipcRenderer.on('message', function (event, text) {
+   $("#update").addClass("visible");
+
+   if (text == 'updateAvailable') {
+      text = "Es ist ein Update verfügbar."
+   } else if (text == 'updateDownloaded') {
+      text = "Update erfolgreich heruntergeladen..."
+   }
+
+   $("#update").html(text);
 })
 
-ipcRenderer.on('downloading', function(event, percent){
-   $("#test").html(percent + "%");
+ipcRenderer.on('downloading', function (event, percent) {
+   $("#update").addClass("visible");
+   $("#update").html(percent + "%");
 })
