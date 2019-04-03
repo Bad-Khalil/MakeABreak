60af66a7dcb396656c11cdb9aabae9ad503a0486
Michael Lucas <info@michael-lucas.net>
2019-04-03 14:44:14 +0200

    Update js

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,26 +18,9 @@ $(function () {
       if (text == 'reload') {
          init()
       }
-   })
-
-   ipcRenderer.on('message', function (event, text) {
-      $("#update").addClass("visible");
-
-      if (text == 'updateAvailable') {
-         text = "Es ist ein Update verfügbar."
-      } else if (text == 'updateDownloaded') {
-         text = "Update erfolgreich heruntergeladen..."
-      }
-
-      $("#update").html(text);
-   })
+   })  
 
    ipcRenderer.on('getVersion', function (event, version) {
       $("#version").html(version)
-   })
-
-   ipcRenderer.on('downloading', function (event, percent) {
-      $("#update").addClass("visible");
-      $("#update").html("Lade Update: " + roundTo(percent, 1) + "%");
    })
 })
---
+++ assets/js/update.js
@@ -0,0 +1,16 @@
+ipcRenderer.on('message', function (event, text) {
+    $("#update").addClass("visible");
+
+    if (text == 'updateAvailable') {
+       text = "Es ist ein Update verfügbar."
+    } else if (text == 'updateDownloaded') {
+       text = "Update erfolgreich heruntergeladen..."
+    }
+
+    $("#update").html(text);
+ })
+
+ ipcRenderer.on('downloading', function (event, percent) {
+    $("#update").addClass("visible");
+    $("#update").html("Lade Update: " + roundTo(percent, 1) + "%");
+ })
