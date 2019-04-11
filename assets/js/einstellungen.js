90a202e2871c88df4a8d5b44b588ec7049093694
Michael Lucas <info@michael-lucas.net>
2019-04-11 18:04:40 +0200

    Quickselect

--- assets/js/einstellungen.js
+++ assets/js/einstellungen.js
@@ -1,9 +1,53 @@
-const {
-    ipcRenderer
-} = require('electron')
+const {ipcRenderer} = require('electron')
 const Store = require('electron-store')
-const settingsStore = new Store()
 const mlMath = require('ml-math')
+const settingsStore = new Store()
+
+function checkIfNumeric(min, sec) {
+    let status = $("#status")
+    if (!mlMath.numbers.isNumeric(min)) {
+        if (mlMath.numbers.isNumeric(sec)) {
+            min = 0
+            $("#timeMin").val('0')
+        } else {
+            status.addClass('keinErfolg')
+            status.html("Ungültige Zahl!")
+            return false
+        }
+    }
+
+    if (!mlMath.numbers.isNumeric(sec)) {
+        if (mlMath.numbers.isNumeric(min)) {
+            sec = 0
+            $("#timeSec").val('0')
+        } else {
+            status.addClass('keinErfolg')
+            status.html("Ungültige Zahl!")
+            return false
+        }
+    }
+
+    return true
+}
+
+function save() {
+    let min = $("#timeMin").val()
+    let sec = $("#timeSec").val()
+    let status = $("#status")
+    status.removeClass()
+    status.addClass("animated bounceIn alert alert-primary")
+
+    if (!checkIfNumeric(min, sec)) {
+        return
+    }
+
+    status.addClass('erfolg')
+    settingsStore.set('timeMin', min)
+    settingsStore.set('timeSec', sec)
+
+    status.html("Erfolgreich gespeichert.")
+    ipcRenderer.send('settingsGespeichert')
+}
 
 $(function () {
     let timeMin = settingsStore.get('timeMin')
@@ -18,40 +62,14 @@ $("#timeMin").change(function () {
     $("#status").html("Setze hier deine Werte.")
 })
 
+
 $("#btnSpeichern").click(function () {
-    ipcRenderer.send('resizeSettings')
-    let min    = $("#timeMin").val()
-    let sec    = $("#timeSec").val()
-    let status = $("#status")
-    status.removeClass()
-    status.addClass("animated bounceIn alert alert-primary")
+    save()
+})
 
-    if (!mlMath.numbers.isNumeric(min)){
-        if (mlMath.numbers.isNumeric(sec)){
-            min = 0
-            $("#timeMin").val('0')
-        }else{
-            status.addClass('keinErfolg')
-            status.html("Ungültige Zahl!")
-            return; 
-        }
-    }
-
-    if (!mlMath.numbers.isNumeric(sec)){
-        if (mlMath.numbers.isNumeric(min)){
-            sec = 0
-            $("#timeSec").val('0')
-        }else{
-            status.addClass('keinErfolg')
-            status.html("Ungültige Zahl!")
-            return; 
-        }
-    }
-
-    status.addClass('erfolg')
-    settingsStore.set('timeMin', min)
-    settingsStore.set('timeSec', sec)
-
-    status.html("Erfolgreich gespeichert.")
-    ipcRenderer.send('settingsGespeichert')
+$("#quickSelect").change(function () {
+    const min = $("#quickSelect").val()
+    $("#timeSec").val('')
+    $("#timeMin").val(min)
+    save()
 })
